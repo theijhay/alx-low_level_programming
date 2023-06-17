f656fc420f5a173e21ef689d85ab66f3d21fabdd
theijhay <olawaleisaacjohn@gmail.com>
2023-06-17 14:10:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
