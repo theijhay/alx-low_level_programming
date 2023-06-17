662d7126d2909c0f6997afac503a91945f0aec08
theijhay <olawaleisaacjohn@gmail.com>
2023-06-17 13:16:46 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Entry point
- *Discription: 'Check for number if it is positive or nagagive'
- *Return: always 0 (Success)
+ * main - assigns a random number and tell if it is positive, nagative or zero
+ *
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else
-	{
-		printf("%d is nagative\n", n);
-	}
-
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is nagative\n", n);
+return (0);
 }
