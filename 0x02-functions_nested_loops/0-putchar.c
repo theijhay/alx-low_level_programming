619fed1d474ee2dbbbba3df768ddbf2b52fc6166
theijhay <olawaleisaacjohn@gmail.com>
2023-06-20 14:16:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My C function readme file
