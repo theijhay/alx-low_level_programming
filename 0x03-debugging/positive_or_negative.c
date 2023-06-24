f5dc6295ff32f43ef586e037f0db6d6c9125c0ee
theijhay <olawaleisaacjohn@gmail.com>
2023-06-23 23:12:28 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	printf("%d is position\n", i);
+	else if (i < 0)
+	printf("%d is negative\n", i);
+	else
+	printf("%d is zero\n", i);
+}
