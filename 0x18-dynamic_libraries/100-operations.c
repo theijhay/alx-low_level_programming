be02e7c5918beb1a1266b654dfdab764e32e7d0f
theijhay <olawaleisaacjohn@gmail.com>
2023-10-16 16:52:06 -0700

    Updated

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,44 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
