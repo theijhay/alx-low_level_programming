bd676e7a76b60d509336025398082055f2dcd10b
theijhay <olawaleisaacjohn@gmail.com>
2023-10-16 17:00:04 -0700

    Updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -shared -o 4libc.so
-LD_PRELOAD=$PWD/4libc.so
+wget -P /tmp https://github.com/theijhay/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
+export LD_PRELOAD=/tmp/injectcode.so
